99500f2ec22d8216aa5870731923d7607859702f
Yigit Gunduc <ygunduc@gmail.com>
2021-11-17 11:12:20 +0300

    textures made better

--- public/index.js
+++ public/index.js
@@ -1,14 +1,14 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
-import Stats from 'three/examples/jsm/libs/stats.module'
-//import { GUI } from 'three/examples/jsm/libs/dat.gui.module'
 
 // CONSTANTS
 const FOV = 75;
 const X_MAX = 128;
 const Y_MAX = 128;
 const TREES = true;
-const MAX_HEIGHT = 12;
+const SEA_LEVEL = -10;
+const SNOW_LEVEL = 13;
+const MAX_HEIGHT = 25;
 const view_distance = 1024;
 const WIDTH = window.innerWidth;
 const HEIGHT = window.innerHeight;
@@ -29,9 +29,11 @@ scene.add(light);
 scene.add(directionalLight);
 
 // Textures
+const woodTexture = new THREE.TextureLoader().load('textures/wood/woodTexture.jpg');
 const snowTexture = new THREE.TextureLoader().load('textures/snow/snowTexture.png');
-const waterTexture = new THREE.TextureLoader().load('textures/water/waterTexture.jpg');
 const grassTexture = new THREE.TextureLoader().load('textures/grass/grassUpFace.jpg');
+const waterTexture = new THREE.TextureLoader().load('textures/water/waterTexture.jpg');
+const leaveTexture = new THREE.TextureLoader().load('textures/leaves/leaveTexture.png');
 
 // CAMERA AND CONTROLS
 const camera = new THREE.PerspectiveCamera(FOV, aspectRatio, 0.1, view_distance);
@@ -40,12 +42,12 @@ controls.update();
 
 camera.position.z = Y_MAX / 2;
 camera.position.x = X_MAX / 2;
-camera.position.y = MAX_HEIGHT;
+camera.position.y = 30;
 camera.lookAt((X_MAX / 2), (Y_MAX / 2), MAX_HEIGHT);
 
-function addVoxel(posx, posy, posz, color) {
+function addVoxel(posx, posy, posz, texture) {
   var geometry = new THREE.BoxGeometry(1, 1, 1);
-  var material = new THREE.MeshLambertMaterial( {color: color} );
+  var material = new THREE.MeshLambertMaterial( {map: texture} );
   var cube = new THREE.Mesh(geometry,material);
   cube.position.set(posx, posy, posz)
   scene.add(cube);
@@ -57,55 +59,54 @@ function addTree(x, height, y) {
 
   height = height -1;
 
-  addVoxel(x, height + 1 , y, 0x964B00);
-  addVoxel(x, height + 2, y,  0x964B00);
-  addVoxel(x, height + 3, y,  0x964B00);
-  addVoxel(x, height + 4, y,  0x964B00);
-  addVoxel(x + 1, height + 5, y,  0x2D5A27);
-  addVoxel(x + 2, height + 5, y,  0x2D5A27);
-  addVoxel(x + 1, height + 5, y + 1,  0x2D5A27);
-  addVoxel(x - 1, height + 5, y - 1,  0x2D5A27);
-  addVoxel(x - 1, height + 5, y + 1,  0x2D5A27);
-  addVoxel(x + 1, height + 5, y - 1,  0x2D5A27);
-  addVoxel(x - 1, height + 5, y,  0x2D5A27);
-  addVoxel(x - 2, height + 5, y,  0x2D5A27);
-  addVoxel(x, height + 5, y + 1,  0x2D5A27);
-  addVoxel(x, height + 5, y + 2,  0x2D5A27);
-  addVoxel(x, height + 5, y - 1,  0x2D5A27);
-  addVoxel(x, height + 5, y - 2,  0x2D5A27);
-  addVoxel(x, height + 6, y,  0x2D5A27);
+  addVoxel(x, height + 1 , y, woodTexture);
+  addVoxel(x, height + 2, y,  woodTexture);
+  addVoxel(x, height + 3, y,  woodTexture);
+  addVoxel(x, height + 4, y,  woodTexture);
+  addVoxel(x + 1, height + 5, y,  leaveTexture);
+  addVoxel(x + 2, height + 5, y,  leaveTexture);
+  addVoxel(x + 1, height + 5, y + 1,  leaveTexture);
+  addVoxel(x - 1, height + 5, y - 1,  leaveTexture);
+  addVoxel(x - 1, height + 5, y + 1,  leaveTexture);
+  addVoxel(x + 1, height + 5, y - 1,  leaveTexture);
+  addVoxel(x - 1, height + 5, y,  leaveTexture);
+  addVoxel(x - 2, height + 5, y,  leaveTexture);
+  addVoxel(x, height + 5, y + 1,  leaveTexture);
+  addVoxel(x, height + 5, y + 2,  leaveTexture);
+  addVoxel(x, height + 5, y - 1,  leaveTexture);
+  addVoxel(x, height + 5, y - 2,  leaveTexture);
+  addVoxel(x, height + 6, y,  leaveTexture);
 
 }
 
-function generatePerlinNoise2D(x, y, max_height=8) {
-  var value = noise.simplex2(x / 50, y / 50);
+function generatePerlinNoise2D(x, y, max_height) {
+  var value = noise.simplex2(x / 125, y / 125);
   value = value * max_height;
   value = Math.floor(value);
 
-  return value
-
+  return value;
 }
 
 function generateTerrain() {
-  for(let x = -(X_MAX / 2); x < (X_MAX / 2); x++) {
-    for(let y = -(Y_MAX / 2); y < (Y_MAX / 2); y++) {
+  for(let x = - (X_MAX / 2); x < (X_MAX / 2); x++) {
+    for(let y = - (Y_MAX / 2); y < (Y_MAX / 2); y++) {
 
       let height = generatePerlinNoise2D(x, y, MAX_HEIGHT);
 
-      if(height >= 6) {
-        let color = 0xffffff;
-        addVoxel(x, height, y, color);
-      } else if (height < -4) {
-        let color = 0x0000ff;
-        addVoxel(x, -4, y, color);
+      if(height >= SNOW_LEVEL) {
+        let texture = snowTexture;
+        addVoxel(x, height, y, texture);
+      } else if (height < SEA_LEVEL) {
+        let texture = waterTexture;
+        addVoxel(x, SEA_LEVEL, y, texture);
       } else { 
-        let color = 0x00ff00;
+        let texture = grassTexture;
         if (TREES) {
-          if (Math.random() > 0.996) {
+          if (Math.random() > 0.97) {
           addTree(x, height, y)
           }
         }
-        addVoxel(x, height, y, color);
+        addVoxel(x, height, y, texture);
       }
     }
   }
@@ -116,7 +117,6 @@ function render() {
   requestAnimationFrame(render);
   controls.update();
 }
-
 
 generateTerrain();
 
