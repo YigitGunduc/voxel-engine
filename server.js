0e51612ac03fe5ce3ad6d3e471dcdbcedb633f1a
Yigit Gunduc <ygunduc@gmail.com>
2021-11-22 16:27:09 +0300

    server uses port 5000 or env.port

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
+const path = require('path')
 const express = require('express')
 const app = express()
-const path = require('path')
+
+const PORT = process.env.PORT || 5000
 
 app.use(express.static(__dirname + '/public'))
 app.use('/build/', express.static(path.join(__dirname, 'node_modules/three/build')))
 app.use('/jsm/', express.static(path.join(__dirname, 'node_modules/three/examples/jsm')))
 
-app.listen(3000, () => console.log('Visit http://127.0.0.1:3000'))
+app.listen(PORT, () => console.log('server is running @ http://127.0.0.1:5000'))
